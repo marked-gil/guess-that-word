da59ab3e8d4a84514656845f2946f8d91787ba9d
marked-gil <markgil_creates@yahoo.com>
2022-07-26 03:02:13 +0000

    Program the game to continue until 10 word definitions have been displayed

--- run.py
+++ run.py
@@ -69,7 +69,7 @@ def get_random_word(used_words_list):
 def display_placeholder(placeholder, full_answer=False):
     """
     Displays the random word placeholder in the terminal, and
-    accepts a boolean argument on whether the full answer is 
+    accepts a boolean argument on whether the full answer is
     to be shown or not
     """
     if full_answer:
@@ -117,36 +117,43 @@ print(LOGO)
 # Holds the IDs of used words
 used_words = []
 
-dictionary_item = get_random_word(used_words)
-used_words.append(dictionary_item[1])
-word_definition = dictionary_item[0]['definition']
-word_to_guess = dictionary_item[0]['word'].upper()
+game_on = True
 
-print(f"Definition:\n\"{word_definition}\"")
+while game_on:
+    dictionary_item = get_random_word(used_words)
+    used_words.append(dictionary_item[1])
+    word_definition = dictionary_item[0]['definition']
+    word_to_guess = dictionary_item[0]['word'].upper()
 
-word_placeholder = ["___" for _ in range(len(word_to_guess))]
-print('  '.join(word_placeholder))
+    print(f"Definition:\n\"{word_definition}\"")
 
-not_guessed_yet = True
-num_guess = 0
+    word_placeholder = ["___" for _ in range(len(word_to_guess))]
+    print('  '.join(word_placeholder))
 
-while not_guessed_yet and num_guess != 3:
-    guess = input("Provide your guess:\n").upper()
+    not_guessed_yet = True
+    num_guess = 0
 
-    if guess == word_to_guess:
-        not_guessed_yet = False
-        print("Congratulations!")
-    else:
-        num_guess += 1
-        if num_guess == 1:
-            word_placeholder = give_1st_hint(word_to_guess, word_placeholder)
-            display_placeholder(word_placeholder)
-            print("Try again!")
-        elif num_guess == 2:
-            word_placeholder = give_2nd_hint(word_to_guess, word_placeholder)
-            display_placeholder(word_placeholder)
-            print("Try again!")
+    while not_guessed_yet and num_guess != 3:
+        guess = input("Provide your guess:\n").upper()
+
+        if guess == word_to_guess:
+            not_guessed_yet = False
+            print("Congratulations!")
         else:
-            print("Sorry your guess is wrong!")
-            answer = show_answer(word_to_guess, word_placeholder)
-            display_placeholder(word_placeholder, True)
+            num_guess += 1
+            if num_guess == 1:
+                word_placeholder = give_1st_hint(word_to_guess, word_placeholder)
+                display_placeholder(word_placeholder)
+                print("Try again!")
+            elif num_guess == 2:
+                word_placeholder = give_2nd_hint(word_to_guess, word_placeholder)
+                display_placeholder(word_placeholder)
+                print("Try again!")
+            else:
+                print("Sorry your guess is wrong!")
+                answer = show_answer(word_to_guess, word_placeholder)
+                display_placeholder(word_placeholder, True)
+
+    if len(used_words) == 10:
+        print("GAME OVER!")
+        game_on = False
