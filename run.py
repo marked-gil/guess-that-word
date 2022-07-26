6059ee5ef9fab6f67926c3c79de00e787964bee6
marked-gil <markgil_creates@yahoo.com>
2022-07-26 01:48:25 +0000

    Add give_1st_hint function

--- run.py
+++ run.py
@@ -57,10 +57,22 @@ WORDS_DICTIONARY = {
 
 def get_random_word():
     """
-    Gets a random word from the words dictionary and return it
+    Gets a random word from the words dictionary and returns it
     """
     word_id = math.floor((random() * len(WORDS_DICTIONARY)+1))
     return WORDS_DICTIONARY[word_id]
+
+
+def give_1st_hint(word, placeholder):
+    """
+    Gives hint by adding the first and last letters of the word to 
+    the placeholder and returns the modified placeholder
+    """
+    first_letter = word[0]
+    last_letter = word[-1]
+    placeholder[0] = first_letter
+    placeholder[-1] = last_letter
+    return placeholder
 
 
 print(LOGO)
@@ -74,12 +86,17 @@ word_placeholder = ["___" for _ in range(len(word_to_guess))]
 print('  '.join(word_placeholder))
 
 not_guessed_yet = True
+num_guess = 0
 
-while not_guessed_yet:
+while not_guessed_yet and num_guess != 3:
     guess = input("Provide your guess:\n").upper()
 
     if guess == word_to_guess:
         not_guessed_yet = False
         print("Congratulations!")
     else:
-        print("Try again!")
+        num_guess += 1
+        if num_guess == 1:
+            word_placeholder = give_1st_hint(word_to_guess, word_placeholder)
+            print('  '.join(word_placeholder))
+            print("Try again!")
