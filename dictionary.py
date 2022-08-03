171b8752573d7d18647fa456a8936de9f0d23c0b
marked-gil <markgil_creates@yahoo.com>
2022-08-03 08:19:01 +0000

    Add more words, and divide them into easy and hard lists of nested dictionaries

--- dictionary.py
+++ dictionary.py
@@ -2,51 +2,360 @@
 This module contains the words and their definitions that are used in the game
 """
 
-# Dictionary of words to guess
-WORDS_DICTIONARY = {
-    1: {
-        "word": "empathy",
-        "definition": """The action of understanding and vicariously
-        experiencing the feelings, thoughts, and experience of another"""
+# List of easy words
+easy_words = [
+    {
+        "id": "e1",
+        "word": "anger",
+        "definition": "A strong feeling of displeasure and usually of antagonism",
     },
-    2: {
-        "word": "expatriate",
-        "definition": "To leave one's native country to live elsewhere",
+    {
+        "id": "e2",
+        "word": "answer",
+        "definition": "Something spoken or written in reply to a question",
     },
-    3: {
-        "word": "meticulous",
-        "definition": """Marked by extreme or excessive care in the consideration
-         or treatment of details""",
+    {
+        "id": "e3",
+        "word": "dream",
+        "definition": "A series of thoughts, images, or emotions occurring during sleep",
     },
-    4: {
-        "word": "intuition",
-        "definition": "An innate sense of what is true or what will happen",
+    {
+        "id": "e4",
+        "word": "exercise",
+        "definition": "Bodily exertion for the sake of developing and maintaining physical fitness",
     },
-    5: {
-        "word": "warehouse",
-        "definition": """""A structure or room for the storage of merchandise or
-        commodities""",
+    {
+        "id": "e5",
+        "word": "famous",
+        "definition": "Widely known",
     },
-    6: {
-        "word": "bailiwick",
-        "definition": """The sphere in which one has superior knowledge or
-        authority""",
+    {
+        "id": "e6",
+        "word": "holiday",
+        "definition": "A day on which one is exempt from work",
     },
-    7: {
+    {
+        "id": "e7",
+        "word": "office",
+        "definition": "A position of responsibility or some degree of executive authority",
+    },
+    {
+        "id": "e8",
+        "word": "wedding",
+        "definition": "A marriage ceremony usually with its accompanying festivities",
+    },
+    {
+        "id": "e9",
+        "word": "student",
+        "definition": "An attentive and systematic observer",
+    },
+    {
+        "id": "e10",
+        "word": "shallow",
+        "definition": "Having little depth",
+    },
+    {
+        "id": "e11",
+        "word": "village",
+        "definition": "A settlement usually larger than a hamlet and smaller than a town",
+    },
+    {
+        "id": "e12",
+        "word": "market",
+        "definition": "A meeting together of people for the purpose of trade by private purchase and sale and usually not by auction",
+    },
+    {
+        "id": "e13",
+        "word": "hammer",
+        "definition": "A hand tool consisting of a solid head set crosswise on a handle and used for pounding",
+    },
+    {
+        "id": "e14",
+        "word": "freedom",
+        "definition": "The absence of necessity, coercion, or constraint in choice or action",
+    },
+    {
+        "id": "e15",
+        "word": "expensive",
+        "definition": "Involving high cost or sacrifice",
+    },
+    {
+        "id": "e16",
+        "word": "comfortable",
+        "definition": "Affording or enjoying contentment and security",
+    },
+    {
+        "id": "e17",
+        "word": "friendly",
+        "definition": "Showing kindly interest and goodwill",
+    },
+    {
+        "id": "e18",
+        "word": "newspaper",
+        "definition": "A paper that is printed and distributed usually daily or weekly and that contains news, articles of opinion, features, and advertising",
+    },
+    {
+        "id": "e19",
+        "word": "problem",
+        "definition": "A question raised for inquiry, consideration, or solution",
+    },
+    {
+        "id": "e20",
+        "word": "substance",
+        "definition": "A fundamental or characteristic part or quality",
+    },
+    {
+        "id": "e21",
+        "word": "special",
+        "definition": "Readily distinguishable from others of the same category",
+    },
+    {
+        "id": "e22",
+        "word": "tomorrow",
+        "definition": "on or for the day after today",
+    },
+    {
+        "id": "e23",
+        "word": "repeat",
+        "definition": "To make, do, or perform again",
+    },
+    {
+        "id": "e24",
+        "word": "silence",
+        "definition": "Absence of sound or noise",
+    },
+    {
+        "id": "e25",
+        "word": "strange",
+        "definition": "Different from what is usual, ordinary, or expected",
+    },
+    {
+        "id": "e26",
+        "word": "promise",
+        "definition": "A declaration that one will do or refrain from doing something specified",
+    },
+    {
+        "id": "e27",
+        "word": "garden",
+        "definition": "A plot of ground where herbs, fruits, flowers, or vegetables are cultivated",
+    },
+    {
+        "id": "e28",
+        "word": "destroy",
+        "definition": "To ruin the structure, organic existence, or condition of",
+    },
+    {
+        "id": "e29",
+        "word": "basket",
+        "definition": "A receptacle made of interwoven material",
+    },
+    {
+        "id": "e30",
+        "word": "attack",
+        "definition": "To set upon or work against forcefully",
+    },
+    {
+        "id": "e31",
+        "word": "fascism",
+        "definition": """A tendency toward or actual exercise of strong autocratic or dictatorial control""",
+    },
+    {
+        "id": "e32",
         "word": "magazine",
         "definition": "A print periodical containing miscellaneous pieces",
     },
-    8: {
-        "word": "fascism",
-        "definition": """A tendency toward or actual exercise of strong autocratic
-         or dictatorial control""",
+    {
+        "id": "e33",
+        "word": "warehouse",
+        "definition": """""A structure or room for the storage of merchandise or commodities""",
     },
-    9: {
+    {
+        "id": "e34",
+        "word": "honest",
+        "definition": "Free from fraud or deception",
+    },
+    {
+        "id": "e35",
+        "word": "accomplish",
+        "definition": "To bring to completion",
+    },
+]
+
+# List of hard words
+hard_words = [
+    {
+        "id": "h1",
+        "word": "empathy",
+        "definition": """The action of understanding and vicariously experiencing the feelings, thoughts, and experience of another""",
+    },
+    {
+        "id": "h2",
+        "word": "expatriate",
+        "definition": "To leave one's native country to live elsewhere",
+    },
+    {
+        "id": "h3",
+        "word": "meticulous",
+        "definition": """Marked by extreme or excessive care in the consideration or treatment of details""",
+    },
+    {
+        "id": "h4",
+        "word": "intuition",
+        "definition": "An innate sense of what is true or what will happen",
+    },
+    {
+        "id": "h5",
+        "word": "bailiwick",
+        "definition": """The sphere in which one has superior knowledge or authority""",
+    },
+    {
+        "id": "h6",
         "word": "community",
         "definition": "A unified body of individuals",
     },
-    10: {
+    {
+        "id": "h7",
         "word": "swindle",
         "definition": "To obtain money or property by fraud or deceit",
     },
-}
+    {
+        "id": "h8",
+        "word": "acquiesce",
+        "definition": "To accept, comply, or submit tacitly or passively",
+    },
+    {
+        "id": "h9",
+        "word": "ambiguous",
+        "definition": "Capable of being understood in two or more possible senses or ways",
+    },
+    {
+        "id": "h10",
+        "word": "deference",
+        "definition": "Respect and esteem due a superior or an elder",
+    },
+    {
+        "id": "h11",
+        "word": "clandestine",
+        "definition": "Marked by, held in, or conducted with secrecy",
+    },
+    {
+        "id": "h12",
+        "word": "divergent",
+        "definition": "Moving or extending in different directions from a common point",
+    },
+    {
+        "id": "h13",
+        "word": "enigma",
+        "definition": "Something hard to understand or explain",
+    },
+    {
+        "id": "h14",
+        "word": "interactive",
+        "definition": "Involving the actions or input of a user",
+    },
+    {
+        "id": "h15",
+        "word": "epitaph",
+        "definition": "An inscription on or at a tomb or a grave in memory of the one buried there",
+    },
+    {
+        "id": "h16",
+        "word": "magnanimous",
+        "definition": "Showing or suggesting a lofty and courageous spirit",
+    },
+    {
+        "id": "h17",
+        "word": "ostentatious",
+        "definition": "Attracting or seeking to attract attention, admiration, or envy often by gaudiness or obviousness",
+    },
+    {
+        "id": "h18",
+        "word": "reflection",
+        "definition": "The production of an image by or as if by a mirror",
+    },
+    {
+        "id": "h19",
+        "word": "authentic",
+        "definition": "Made or done the same way as an original",
+    },
+    {
+        "id": "h20",
+        "word": "eminent",
+        "definition": "Standing out so as to be readily perceived or noted",
+    },
+    {
+        "id": "h21",
+        "word": "identical",
+        "definition": "Having such close resemblance as to be essentially the same",
+    },
+    {
+        "id": "h22",
+        "word": "augment",
+        "definition": "To make greater, more numerous, larger, or more intense",
+    },
+    {
+        "id": "h23",
+        "word": "courage",
+        "definition": "Mental or moral strength to venture, persevere, and withstand danger, fear, or difficulty",
+    },
+    {
+        "id": "h24",
+        "word": "sufficient",
+        "definition": "Enough to meet the needs of a situation or a proposed end",
+    },
+    {
+        "id": "h25",
+        "word": "extort",
+        "definition": "To obtain from a person by force, intimidation, or undue or illegal power",
+    },
+    {
+        "id": "h26",
+        "word": "wayward",
+        "definition": "Following one's own capricious, wanton, or depraved inclinations",
+    },
+    {
+        "id": "h27",
+        "word": "converge",
+        "definition": "To come together and unite in a common interest or focus",
+    },
+    {
+        "id": "h28",
+        "word": "admiration",
+        "definition": "A feeling of respect and approval",
+    },
+    {
+        "id": "h29",
+        "word": "agreement",
+        "definition": "Harmony of opinion, action, or character",
+    },
+    {
+        "id": "h30",
+        "word": "appropriate",
+        "definition": "Especially suitable or compatible",
+    },
+    {
+        "id": "h31",
+        "word": "calamity",
+        "definition": "A disastrous event marked by great loss and lasting distress and suffering",
+    },
+    {
+        "id": "h32",
+        "word": "obsolete",
+        "definition": "No longer in use or no longer useful",
+    },
+    {
+        "id": "h33",
+        "word": "outskirt",
+        "definition": "A part remote from the center",
+    },
+    {
+        "id": "h34",
+        "word": "invincible",
+        "definition": "Incapable of being conquered, overcome, or subdued",
+    },
+    {
+        "id": "h35",
+        "word": "docile",
+        "definition": "Easily taught",
+    },
+]
