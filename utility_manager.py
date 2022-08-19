fda140cea2564e1e01168cda7bc3ac814dee4285
marked-gil <markgil_creates@yahoo.com>
2022-08-19 18:04:35 +0000

    Update comments in utility_manager.py

--- utility_manager.py
+++ utility_manager.py
@@ -9,19 +9,22 @@ def clear_terminal():
     """
     Clears the terminal
     """
+    # Suggested in stackoverflow. (see Credits section in README)
     print("\033c")
 
 
 def blank_lines(num_lines, line_type="print_line"):
     """
-    Adds specific number of blank lines for styling purposes & has
-    2 parameters: "number of blank lines" and "line type".
+    Adds specific number of blank lines for styling.
+    Parameters: num_lines (number of blank lines), line type
+
     "line_type" accepts "print_line", "inline", and after_line"
-    as arguments. "print_line" (default) if the function is directly placed
-    inside print(); "inline" if function is concatenated in a string but
-    not at the end; and "after_line" if function is concatenated at end
-    of a string. This function eturns the number of blank lines to be
-    printed in the terminal.
+    as arguments.
+    "print_line" (default): - if this is inside print();
+    "inline": - if this is concatenated in a string but not at the end;
+    "after_line": - if this is concatenated at end of a string.
+
+    Returns: number of blank lines that can be printed in the terminal
     """
     if line_type == "print_line":
         num_lines -= 1
@@ -37,4 +40,5 @@ def return_home():
     """
     Refreshes the game and goes back to home
     """
+    # Suggested in stackoverflow. (see Credits section in README)
     os.execv(sys.executable, ['python'] + sys.argv)
